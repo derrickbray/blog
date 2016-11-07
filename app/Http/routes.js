9140d96a1bec59593c5819d0ca71e91ba0acde34
Derrick Bray <derrickbray@yahoo.com>
2016-11-07 10:43:05 -0600

    basic Database

--- app/Http/routes.js
+++ app/Http/routes.js
@@ -15,6 +15,22 @@
 | Route.resource('user', 'UserController')
 */
 
-const Route = use('Route')
+const Route = use('Route');
+const Database = use('Database');
 
-Route.on('/').render('welcome')
+Route.get('/', function * (request, response){
+  yield Database.table('posts').insert({
+    title: 'My first blog post.',
+    author: 'Derrick',
+
+    slug: 'my-first-blog-post',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+
+    post_date: new Date(),
+    created_at: new Date(),
+    updated_at: new Date(),
+  });
+  const data = yield Database.table('posts').select('*');
+
+  response.send(data);
+});
