d9bde0bf952b5d93f8a0629f459733d5761f14f1
Derrick Bray <derrickbray@yahoo.com>
2016-11-07 10:50:11 -0600

    git

--- app/Http/routes.js
+++ app/Http/routes.js
@@ -16,10 +16,10 @@
 */
 
 const Route = use('Route');
-const Database = use('Database');
+const Post = use('App/Model/Post');
 
 Route.get('/', function * (request, response){
-  yield Database.table('posts').insert({
+  const p = yield Post.create({
     title: 'My first blog post.',
     author: 'Derrick',
 
@@ -27,10 +27,10 @@ Route.get('/', function * (request, response){
     content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 
     post_date: new Date(),
-    created_at: new Date(),
-    updated_at: new Date(),
-  });
-  const data = yield Database.table('posts').select('*');
 
-  response.send(data);
+  });
+  // const data = yield Database.table('posts').select('*');
+  //
+  // response.send(data);
+    response.send(p);
 });
